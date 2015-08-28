c48d7cf68f3ddcb63ad4169a8416c270ddc300d1
Deyves Carvalho <deyvescarvalho@gmail.com>
2015-08-28 11:55:38 -0300

    mail

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <?php require_once 'head.php';?>
     <div class="row col-md-6 col-md-offset-3">
-        <form>
+        <form method="post" action="enviarmail.php">
             <div class="form-group">
                 <label for="exampleInputEmail1">E-mail</label>
-                <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
+                <input type="email" class="form-control" name="email id="exampleInputEmail1" placeholder="Email">
             </div>
             <div class="form-group">
                 <label for="exampleInputPassword1">Senha</label>
-                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                <input type="password" class="form-control" name="senha" id="exampleInputPassword1" placeholder="Password">
             </div>
             <button type="submit" class="btn btn-default">Submit</button>
         </form>
