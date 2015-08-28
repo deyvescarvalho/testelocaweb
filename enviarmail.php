dfdb1012c8fbd14c1781a2bbc2b8d1e9a77a96c2
Deyves Carvalho <deyvescarvalho@gmail.com>
2015-08-28 18:13:45 -0300

    enviar mail

--- enviarmail.php
+++ enviarmail.php
@@ -1,1 +1,10 @@
-<?php echo "Enviando...";?>
+<?php
+$to      = 'deyvescarvalho@gmail.com';
+$subject = 'Teste';
+$message = 'hello';
+$headers = 'From: webmaster@example.com' . "\r\n" .
+    'Reply-To: webmaster@example.com' . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+
+mail($to, $subject, $message, $headers);
+?>
