2e83c62f1f054dcd8ef02a6b374a56fa666c1d9e
Deyves Carvalho <deyvescarvalho@gmail.com>
2015-08-28 18:21:21 -0300

    mail

--- enviarmail.php
+++ enviarmail.php
@@ -6,10 +6,12 @@ $headers = 'From: webmaster@example.com' . "\r\n" .
     'Reply-To: webmaster@example.com' . "\r\n" .
     'X-Mailer: PHP/' . phpversion();
 
-
+mail
 
 if(mail($to, $subject, $message, $headers)){
     echo "Mensagem enviada com sucesso";
+}else{
+
+    echo "Falha no envio do email";
 }
-echo "Falha no envio do email";
 ?>
