1c8160b685609d3b0cad67c418ff0c9ec6634437
Deyves Carvalho <deyvescarvalho@gmail.com>
2015-09-01 19:52:14 -0300

    estn

--- enviarmail.php
+++ enviarmail.php
@@ -1,49 +1,26 @@
 <?php
 
-$url = 'https://api.sendgrid.com/';
-$user = 'deyves';
-$pass = 'q13791919';
+$sendgrid = new SendGrid('deyves', 'q13791919');
+$email = new SendGrid\Email();
+$email
+    ->addTo('deyvescarvalho@gmail.com')
+    ->setFrom('deyvescarvalho@gmail.com')
+    ->setSubject('Subject goes here')
+    ->setText('Hello World!')
+    ->setHtml('<strong>Hello World!</strong>')
+;
 
-$json_string = array(
+$sendgrid->send($email);
 
-    'to' => array(
-        'deyvescarvalho@gmail.com'
-    ),
-    'category' => 'Assunto'
-);
+// Or catch the error
 
-
-$params = array(
-    'api_user'  => $user,
-    'api_key'   => $pass,
-    'x-smtpapi' => json_encode($json_string),
-    'to'        => 'deyvescarvalho@gmail.com',
-    'subject'   => 'testing from curl',
-    'html'      => 'testing body',
-    'text'      => 'testing body',
-    'from'      => 'deyvescarvalho@gmail.com',
-);
-
-
-$request =  $url.'api/mail.send.json';
-
-// Generate curl request
-$session = curl_init($request);
-// Tell curl to use HTTP POST
-curl_setopt ($session, CURLOPT_POST, true);
-// Tell curl that this is the body of the POST
-curl_setopt ($session, CURLOPT_POSTFIELDS, $params);
-// Tell curl not to return headers, but do return the response
-curl_setopt($session, CURLOPT_HEADER, false);
-// Tell PHP not to use SSLv3 (instead opting for TLS)
-curl_setopt($session, CURLOPT_SSLVERSION, CURL_SSLVERSION_TLSv1_2);
-curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
-
-// obtain response
-$response = curl_exec($session);
-curl_close($session);
-
-// print everything out
-print_r($response);
+try {
+    $sendgrid->send($email);
+} catch(\SendGrid\Exception $e) {
+    echo $e->getCode();
+    foreach($e->getErrors() as $er) {
+        echo $er;
+    }
+}
 
 ?>
