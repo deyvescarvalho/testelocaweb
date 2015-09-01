258131cdc592f6368f56c84f2e48fb94a38d0aea
Deyves Carvalho <deyvescarvalho@gmail.com>
2015-09-01 18:06:33 -0300

    use sendgrid

--- enviarmail.php
+++ enviarmail.php
@@ -1,15 +1,39 @@
 <?php
-$to      = 'deyvescarvalho@gmail.com';
-$subject = 'Teste';
-$message = 'hello';
-$headers = 'From: webmaster@example.com' . "\r\n" .
-    'Reply-To: webmaster@example.com' . "\r\n" .
-    'X-Mailer: PHP/' . phpversion();
 
-if(mail($to, $subject, $message, $headers)){
-    echo "Mensagem enviada com sucesso";
-}else{
+$url = 'https://api.sendgrid.com/';
+$user = 'deyves';
+$pass = 'q13791919';
 
-    echo "Falha no envio do email";
-}
+$params = array(
+    'api_user'  => $user,
+    'api_key'   => $pass,
+    'to'        => 'deyvescarvalho@gmail.com',
+    'subject'   => 'testing from curl',
+    'html'      => 'testing body',
+    'text'      => 'testing body',
+    'from'      => 'example@sendgrid.com',
+);
+
+
+$request =  $url.'api/mail.send.json';
+
+// Generate curl request
+$session = curl_init($request);
+// Tell curl to use HTTP POST
+curl_setopt ($session, CURLOPT_POST, true);
+// Tell curl that this is the body of the POST
+curl_setopt ($session, CURLOPT_POSTFIELDS, $params);
+// Tell curl not to return headers, but do return the response
+curl_setopt($session, CURLOPT_HEADER, false);
+// Tell PHP not to use SSLv3 (instead opting for TLS)
+curl_setopt($session, CURLOPT_SSLVERSION, CURL_SSLVERSION_TLSv1_2);
+curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
+
+// obtain response
+$response = curl_exec($session);
+curl_close($session);
+
+// print everything out
+print_r($response);
+
 ?>
