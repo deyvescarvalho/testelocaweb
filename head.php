9b4f3b06da4707b62ee31ad42ed380a99459defa
Deyves Carvalho <deyvescarvalho@gmail.com>
2015-09-02 03:10:33 -0300

    testes responsivos

--- head.php
+++ head.php
@@ -4,14 +4,18 @@
     <meta charset="UTF-8">
     <title>Minha Página</title>
     <link rel="stylesheet"  href="bower_components/bootstrap/dist/css/bootstrap.css">
-    <meta name="viewport" content="width=device-width, user-scalable=no" />
+    <link rel="stylesheet"  href="responsivemobilemenu/rmm-css/responsivemobilemenu.css">
+    <script type="text/javascript" src="http://code.jquery.com/jquery.min.js"></script>
+    <script type="text/javascript" src="responsivemobilemenu/rmm-js/responsivemobilemenu.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"/>
 </head>
 <body>
+<nav class="rmm ">
+    <ul class="nav nav-pills">
+        <li role="presentation"><a href="index.php">Início</a></li>
+        <li role="presentation"><a href="formulario.php">Contato</a></li>
+        <li role="presentation"><a href="trabalho.php">Trabalho</a></li>
+    </ul>
+</nav>
 <div class="container">
-    <nav class="row">
-        <ul class="nav nav-pills">
-            <li role="presentation"><a href="index.php">Início</a></li>
-            <li role="presentation"><a href="formulario.php">Contato</a></li>
-            <li role="presentation"><a href="trabalho.php">Trabalho</a></li>
-        </ul>
-    </nav>
+
