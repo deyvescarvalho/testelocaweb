6d920e4edcf37b797e750cc44b64e1a762b7b944
Deyves Carvalho <deyvescarvalho@gmail.com>
2015-09-02 01:38:05 -0300

    Teste manual com mail

--- enviarmail.php
+++ enviarmail.php
@@ -1,26 +1,54 @@
+
 <?php
-require("sendgrid-php/sendgrid-php.php");
+require_once 'head.php';
+$nome = $_POST['nome'];
+$email = $_POST['email'];
+$cidade = $_POST['cidade'];
+$estado = $_POST['estado'];
+$rua = $_POST['rua'];
+$mensagem = $_POST['mensagem'];
 
-$sendgrid = new SendGrid('deyves', 'q13791919');
-$email = new SendGrid\Email();
-$email
-    ->addTo('deyvescarvalho@gmail.com')
-    ->setFrom('deyvescarvalho@gmail.com')
-    ->setSubject('Subject goes here')
-    ->setText('Hello World!')
-    ->setHtml('<strong>Hello World!</strong>')
-;
+/*////*/
+$to = "deyvescarvalho@gmail.com";
+$subject = "Testando novo modo";
+$message = "Nome: ". $nome . "<br/>";
+$message .= "Email: ". $email . "<br/>";
+$message .= "Cidade: ". $cidade . "<br/>";
+$message .= "Estado: ". $estado . "<br/>";
+$message .= "Rua: ". $rua . "<br/>";
+$message .= "Mensagem: ". $mensagem . "<br/>";
+$header = "MIME-Version: 1.0\n";
+$header = "Content-type: text/html; charset=iso-8859-1\n";
+$header = "From: $email\n";
 
-$sendgrid->send($email);
 
-// Or catch the error
+mail($to, $subject, $message, $header);
 
-try {
-    $sendgrid->send($email);
-} catch(\SendGrid\Exception $e) {
-    echo $e->getCode();
-    foreach($e->getErrors() as $er) {
-        echo $er;
-    }
-}
+echo "Mensagem enviada com Sucesso...";
+
+
+//require("sendgrid-php/sendgrid-php.php");
+//
+//$sendgrid = new SendGrid('deyves', 'q13791919');
+//$email = new SendGrid\Email();
+//$email
+//    ->addTo('deyvescarvalho@gmail.com')
+//    ->setFrom('deyvescarvalho@gmail.com')
+//    ->setSubject('Subject goes here')
+//    ->setText('Hello World!')
+//    ->setHtml('<strong>Hello World!</strong>')
+//;
+//
+//$sendgrid->send($email);
+//
+//// Or catch the error
+//
+//try {
+//    $sendgrid->send($email);
+//} catch(\SendGrid\Exception $e) {
+//    echo $e->getCode();
+//    foreach($e->getErrors() as $er) {
+//        echo $er;
+//    }
+//}
 ?>
--- formulario.php
+++ formulario.php
@@ -1,15 +1,34 @@
 <?php require_once 'head.php';?>
-    <div class="row col-md-6 col-md-offset-3">
+    <div class="row col-md-5 col-md-offset-3">
         <form method="post" action="enviarmail.php">
-            <div class="form-group">
-                <label for="exampleInputEmail1">E-mail</label>
-                <input type="email" class="form-control" name="email id="exampleInputEmail1" placeholder="Email">
+            <div class="form-group col-xs-12">
+                <label for="nome">Nome:</label>
+                <input type="text" class="form-control" name="nome" id="nome">
             </div>
-            <div class="form-group">
-                <label for="exampleInputPassword1">Senha</label>
-                <input type="password" class="form-control" name="senha" id="exampleInputPassword1" placeholder="Password">
+            <div class="form-group col-xs-12">
+                <label for="email">E-mail</label>
+                <input type="email" class="form-control" name="email" id="email">
             </div>
-            <button type="submit" class="btn btn-default">Submit</button>
+            <div class="form-group col-xs-8">
+                <label for="cidade">Cidade</label>
+                <input type="text" class="form-control" name="cidade" id="cidade">
+            </div>
+            <div class="form-group col-xs-4">
+                <label for="estado">Estado</label>
+                <input type="text" class="form-control" name="estado" id="estado">
+            </div>
+            <div class="form-group col-xs-12">
+                <label for="rua">Rua</label>
+                <input type="text" class="form-control" name="rua" id="rua">
+            </div>
+            <div class="form-group col-xs-12">
+                <textarea name="mensagem"  placeholder="Digite aqui Sua mensagem"  id="mensagem" class="col-xs-10 col-xs-offset-1" rows="5" ></textarea>
+            </div>
+            <div class="form-group col-xs-12">
+                <button type="submit" class="col-xs-3  col-xs-offset-5 btn btn-success">Enviar</button>
+                <button type="reset" class="col-xs-3  col-xs-offset-1 btn btn-danger">Cancelar</button>
+            </div>
+
         </form>
     </div>
 
