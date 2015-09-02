23262f71d3598fce43dd8d2426ca1db23ac0dbbc
Deyves Carvalho <deyvescarvalho@gmail.com>
2015-09-02 03:20:22 -0300

    responsive

--- head.php
+++ head.php
@@ -3,19 +3,22 @@
 <head>
     <meta charset="UTF-8">
     <title>Minha Página</title>
-    <link rel="stylesheet"  href="bower_components/bootstrap/dist/css/bootstrap.css">
-    <link rel="stylesheet"  href="responsivemobilemenu/rmm-css/responsivemobilemenu.css">
     <script type="text/javascript" src="http://code.jquery.com/jquery.min.js"></script>
     <script type="text/javascript" src="responsivemobilemenu/rmm-js/responsivemobilemenu.js"></script>
+    <link rel="stylesheet" href="responsivemobilemenu/rmm-css/responsivemobilemenu.css">
+    <link rel="stylesheet"  href="bower_components/bootstrap/dist/css/bootstrap.css">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"/>
 </head>
 <body>
-<nav class="rmm ">
-    <ul class="nav nav-pills">
-        <li role="presentation"><a href="index.php">Início</a></li>
-        <li role="presentation"><a href="formulario.php">Contato</a></li>
-        <li role="presentation"><a href="trabalho.php">Trabalho</a></li>
+<div class="rmm graphite">
+    <ul>
+        <li><a href='index.php'>Início</a></li>
+        <li><a href='formulario.php'>Contato</a></li>
+        <li><a href='trabalho.php'>Trabalho</a></li>
+        <li><a href='#'>Blog</a></li>
+        <li><a href='#'>Links</a></li>
+        <li><a href='#'>Sitemap</a></li>
     </ul>
-</nav>
+</div>
 <div class="container">
 
